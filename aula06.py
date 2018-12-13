1dc99f3b8bfd5c64b77820c95d3231477d263f2e
Saulo Catharino <saulocatharino@gmail.com>
2018-12-13 18:57:01 -0200

    Update aula06.py

--- aula06.py
+++ aula06.py
@@ -8,7 +8,8 @@ import numpy as np
 existe = os.path.isfile('tickers.csv')
 
 
-janela = 900
+
+janela = 360
 desvio = 2
 
 historico_bid = []
@@ -19,7 +20,11 @@ historico_compras = []
 historico_vendas = []
 historico_sinal = []
 index_compras = []
-index_vendas = []  
+index_vendas = []
+
+fig = plt.figure(figsize=(10,10))
+
+ax = fig.gca()
 if existe:
     pass
 else:
@@ -28,48 +33,31 @@ else:
     grava.close()
 
 
-fig = plt.figure(figsize=(10,10))
-
-ax = fig.gca()
-
-
-
 
 def Bollinger_Bands(bid, ask, janela, desvio):
-
     if len(bid) > janela:
-        media = bid.rolling(window= janela).mean()
-        rolling_std  = bid.rolling(window= janela).std()
+        media = bid.rolling( window = janela).mean()
+        rolling_std = bid.rolling( window = janela).std()
         upper_band = media + (rolling_std * desvio)
         lower_band = media - (rolling_std * desvio)
 
-        porcentagem = spread(bid[-1:],ask[-1:])
+        porcentagem = spread(bid[-1:], ask[-1:])
         diferenca = ask[-1:] - bid[-1:] 
+        print(diferenca)
         ax.text(len(ask) + (len(ask)/10), bid[-1:] + (diferenca/2), "Spread " + str(np.around(float(porcentagem),3)) + "%")
 
+        ax.plot(upper_band, '--', color = "green", alpha = .5)
+        ax.plot(lower_band, '--', color = "red", alpha = .5)
 
-
-        ax.plot(media, '--', color = 'gray', alpha = 0.3)
-        ax.plot(upper_band, '--', color = 'blue', alpha = 0.5)
-        ax.plot(lower_band, '--', color = 'blue', alpha = 0.2)
-
-
-        ax.scatter(len(ask),media[-1:], color = 'gray', alpha = 0.1)
-        ax.scatter(len(ask),upper_band[-1:], color = 'blue', alpha = 0.1)
-        ax.scatter(len(ask),lower_band[-1:], color = 'blue', alpha = 0.1)
+        ax.scatter(len(ask), upper_band[-1:], color = "green", alpha = .1)
+        ax.scatter(len(ask), lower_band[-1:], color = "green", alpha = .1)
         return lower_band, upper_band
-
 
 
     else:
         print("Sem dados suficientes para criar faixas de Bollinger")
 
 
-
-
- 
-
-    
 def detect_cross(bid, ask, lower_band, upper_band, index):
 
     historico_bid.append(bid)
@@ -77,16 +65,14 @@ def detect_cross(bid, ask, lower_band, upper_band, index):
     historico_lower_band.append(lower_band)
     historico_upper_band.append(upper_band)
 
-
     del historico_bid[:-10]
     del historico_ask[:-10]
-    del historico_lower_band[:-10]
     del historico_upper_band[:-10]
-
+    del historico_lower_band[:-10]
 
 
     if len(historico_sinal) > 1:
-
+    
         if historico_bid[-1:] > historico_lower_band[-1:] and historico_bid[-2:-1] <= historico_lower_band[-2:-1]:
             historico_compras.append(float(ask))
             index_compras.append(index)
@@ -97,34 +83,18 @@ def detect_cross(bid, ask, lower_band, upper_band, index):
             historico_vendas.append(float(bid))
             index_vendas.append(index)
             sinal = "Sell"
-            sinal_action =2
+            sinal_action = 2
         else:
             sinal = "Hold"
             sinal_action = 0
+        historico_sinal.append(sinal_action)
+
     else:
         sinal = "Hold"
         sinal_action = 0
-    historico_sinal.append(sinal_action) 
+        historico_sinal.append(sinal_action)
 
     return 0
-
-
-
-def plota_negociatas(bid,ask, lower_band, upper_band):
-    reset_memoria()
-
-    for i in range(len(bid)-(janela*2), len(bid)):            
-        _ = detect_cross(float(bid[i]), float(ask[i]), float(lower_band[i]), float(upper_band[i]), i)
-
-    if len(historico_compras) > 0:
-        ax.scatter(index_compras, historico_compras, marker = 'v', color = "red", label = "Compra")
-        for c in range(len(index_compras)): 
-            ax.text(index_compras[c], historico_compras[c], '- compra', color = "black", alpha = 0.8)
-
-    if len(historico_vendas) > 0:
-        ax.scatter(index_vendas, historico_vendas, marker = '^', color = "green", label = "Venda")
-        for v in range(len(index_vendas)): 
-            ax.text(index_vendas[v], historico_vendas[v], '- venda', color = "black", alpha = 0.8)
 
 
 def reset_memoria():
@@ -135,6 +105,26 @@ def reset_memoria():
     index_compras = []
     index_vendas = []
 
+def plota_negociatas(bid, ask, lower_band, upper_band):
+    reset_memoria()
+    
+    for i in range(len(bid) - (janela*2), len(bid)):
+        _ = detect_cross(float(bid[i]), float(ask[i]), float(lower_band[i]), float(upper_band[i]), i)
+      
+    if len(historico_compras) > 0:
+        ax.scatter(index_compras, historico_compras, marker = 'v', color = 'red')
+        for c in range(len(index_compras)):
+            ax.text(index_compras[c], historico_compras[c], ' - compra', color = 'black', alpha = .5)
+
+
+    if len(historico_vendas) > 0:
+        ax.scatter(index_vendas, historico_vendas, marker = '^', color = 'green')
+        for v in range(len(index_vendas)):
+            ax.text(index_vendas[v], historico_vendas[v], ' - venda', color = 'black', alpha = .5)
+
+
+
+
 def spread(bid,ask):
     porcento = ask / 100
     diferenca = ask - bid 
@@ -144,7 +134,7 @@ def spread(bid,ask):
 
 
 def get_tickers():
-    bitfinex_ltc = "https://api.bitfinex.com/v1/pubticker/ltcbtc"
+    bitfinex_ltc = "https://api.bitfinex.com/v1/pubticker/btcusd"
     data_bitfinex = requests.get(url=bitfinex_ltc)
     binary_bitfinex = data_bitfinex.content
     output_bitfinex = json.loads(binary_bitfinex)
@@ -154,8 +144,11 @@ def get_tickers():
 
 
 
-def plot():
+
+def main():
     df = pd.read_csv("tickers.csv")
+    
+
 
     if len(df) > 1:
         ax.clear()
@@ -165,48 +158,30 @@ def plot():
 
         diferenca = ask[-1:] - bid[-1:]
 
+        plt.title("Bitcoin / USD") 
 
-        plt.title("Litecoin/BTC")
+        if len(bid) < janela *2 :
+            ax.set_xlim(0,len(bid) + (len(bid)/10))
 
-        if len(bid) < janela*2:
-            ax.set_xlim(0, len(bid)+(len(bid)/4)+5)
 
         else:
-            ax.set_xlim(len(bid)-janela*2, len(bid)+100)
+            ax.set_xlim(len(bid)-janela*2, len(bid)+10)
             bid_min = np.array(bid[-janela*2:]).min()
             ask_max = np.array(ask[-janela*2:]).max()
-            ax.set_ylim(bid_min-(bid_min * .001),ask_max+(ask_max * .001))
+            ax.set_ylim(bid_min-(bid_min * .01), ask_max+(ask_max * .01))
+        ax.plot(bid, label = "BID - VENDA BTC " + str(np.around(float(bid[-1:]),2)), color = 'black', alpha = .5)
+        ax.plot(ask, label = "ASK - COMPRA BTC " + str(np.around(float(ask[-1:]),2)), color = 'gray', alpha = .5)
 
 
 
-
-        ax.plot(bid, label = "Bid - Venda LTC "+ str(np.around(float(bid[-1:]),8)), color = 'green', alpha = 0.5)
-        ax.plot(ask, label = "Ask - Compra LTC "+ str(np.around(float(ask[-1:]),8)), color = 'red', alpha = 0.5)
-
-        
-        plt.legend()
-
-        ax.scatter(len(ask)-1,ask[-1:], color = 'red', alpha = 1)
-        ax.scatter(len(bid)-1,bid[-1:], color = 'green', alpha = 1)
-        if len(bid) > janela * 2:
+        if len(bid) > janela *2:
+            print(type(bid),type(ask), type(janela), type(desvio)) 
             lower_band, upper_band = Bollinger_Bands(bid, ask, janela, desvio)
-            plota_negociatas(bid,ask, lower_band, upper_band)
+            plota_negociatas(bid, ask, lower_band, upper_band)
 
         plt.pause(2)
 
 
-
 while True:
-
-    try:
-        get_tickers()
-    except:
-        print("Erro no servidor")
-        time.sleep(5)
-    try:
-        plot()
-    except:
-        print("-- Encerrando o Programa --")
-        exit()
-            
-   
+    get_tickers()
+    main()
